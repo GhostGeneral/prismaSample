0b2338cc9c00433bdb9be993ac429ab12ed1aa97
Afeez Awotundun <awotundun@gmail.com>
2021-12-24 12:36:03 +0100

    Initial commit

---
+++ src/logger.ts
@@ -0,0 +1,38 @@
+import winston from 'winston';
+
+export default class Logger {
+    public static getLogger(label: string): winston.Logger {
+        if (!winston.loggers.has(label)) {
+            winston.loggers.add(label, {
+                transports: Logger.transports,
+                format: winston.format.label({ label }),
+            });
+        }
+        return winston.loggers.get(label);
+    }
+
+    private static logFormat = winston.format.printf(
+        info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`,
+    );
+
+    private static readonly transports = [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.colorize(),
+                Logger.logFormat,
+            ),
+            level: 'debug',
+        }),
+        new winston.transports.File({
+            filename: 'logs/combined.log',
+            format: winston.format.json(),
+            level: 'debug',
+        }),
+        new winston.transports.File({
+            level: 'error',
+            filename: 'logs/error.log',
+            format: winston.format.json(),
+        }),
+    ];
+}
